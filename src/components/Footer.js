86ba802c0c85af2dbcf778a20e51e2bf168e37e8
Furquan712 <smfurquan7.12@gmail.com>
2022-03-28 13:57:59 +0530

    weatherapp sih

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import HeartIcon from "../img/heart.svg";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>
+        {/* <img src= alt="love icon" /> */}
+        &nbsp; by &nbsp;
+        <a href="" target="_blank" rel="noreferrer" >Team Interstellar</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
